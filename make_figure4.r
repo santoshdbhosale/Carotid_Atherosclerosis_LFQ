b55e8fbbddb41a203f565c6b75237872a0edcc3d
Santosh <santoshdbhosale@gmail.com>
2020-09-25 16:48:01 +0200

    Update make_figure4.r

--- make_figure4.r
+++ make_figure4.r
@@ -1,2 +1,5 @@
+## load the data
 df = read.table("FBLN-1C.txt", dec = ".", header = TRUE)
 
+## generate the boxplot
+ggplot(df, aes(x=factor(Condition), y=FBLN1C, fill=Condition)) + geom_boxplot(notch=TRUE)
